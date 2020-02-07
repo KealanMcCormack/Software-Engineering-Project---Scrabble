3a0a110ceb6be3cda06557fad9c5d9faa61f3e21
Lukasz Filanowski <44058072+Lukadolla@users.noreply.github.com>
2020-02-07 10:25:51 +0000

    Add files via upload

--- src/Player.java
+++ src/Player.java
@@ -1,21 +1,21 @@
 /**
- * @author: Lukasz Filanowski
+ * @author Lukasz Filanowski
  * The purpose of this class is to create a player instance for the Scrabble game with
  * a name and score tied to the specific player instance.
  */
 
 public class Player {
     /**
-     * @param name: Name of player
-     * @param score: Score of player
+     * @param name Name of player
+     * @param score Score of player
      * Private instance variables
      */
     private String name;    //Instance variables for player data
     private int score;
 
     /**
-     * @param name: Name of player
-     * @param score: Score of player
+     * @param name Name of player
+     * @param score Score of player
      * Constructor for PLayer
      */
     public Player (String name, int score) //Constructor
