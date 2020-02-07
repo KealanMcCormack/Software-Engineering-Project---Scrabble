3115766157a4f491603f00145e841a55fda7176f
Lukasz Filanowski <44058072+Lukadolla@users.noreply.github.com>
2020-02-07 10:18:22 +0000

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,110 @@
+/**
+ * @author Lukasz Filanowski
+ * The purpose of this class is to create a player instance for the Scrabble game with
+ * a name and score tied to the specific player instance.
+ */
+
+public class Player {
+    /**
+     * @param name Name of player
+     * @param score Score of player
+     * Private instance variables
+     */
+    private String name;    //Instance variables for player data
+    private int score;
+
+    /**
+     * @param name Name of player
+     * @param score Score of player
+     * Constructor for PLayer
+     */
+    public Player (String name, int score) //Constructor
+    {
+        setName(name);
+        setScore(score);
+
+        if(name.isEmpty())   //Exception that's thrown if player doesn't enter name initially (upon instance creation)
+        {
+            throw new IllegalArgumentException("Name is not initialised");
+        }
+
+        if(score != 0)  //Exception that's thrown if score is anything other than 0 (upon instance creation)
+        {
+            throw new IllegalArgumentException("Score has invalid value");
+        }
+    }
+
+    /**
+     * @param name
+     * Setting player name
+     */
+    public void setName(String name)   //Method to set name of player
+    {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @param score
+     * Setting player score
+     */
+    public void setScore(int score)    //Method to set score of player
+    {
+        this.score = score;
+
+        if(score < 0)     //Exception that's thrown if score is less than 0
+        {
+            throw new IllegalArgumentException("Score is an invalid value");
+        }
+    }
+
+    /**
+     *
+     * @return
+     * Returning score of player
+     */
+    public int getScore()      //Method to get score of player
+    {
+        return score;
+    }
+
+    /**
+     *
+     * @return
+     * Returning name of player
+     */
+    public String getName()  //Method to get name of player
+    {
+        return name;
+    }
+
+    /**
+     *
+     * @param increment
+     * Incrementing score of the player
+     */
+    protected void increaseScore(int increment)   //Method that takes in a value to increment the score by and increases the score
+    {
+        setScore((this.score + increment));
+    }
+
+    /**
+     * Resetting score
+     */
+    protected void resetScore()  //Method to reset the score of the player
+    {
+        setScore(0);
+    }
+
+    /**
+     * Resetting name
+     */
+    protected void resetName()  //Method to reset the name of the player
+    {
+        setName("");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ PlayerTest.java
@@ -0,0 +1,31 @@
+/**
+ * @author Lukasz Filanowski
+ * Class to test the methods in the  Player class
+ */
+
+public class PlayerTest {
+
+    public static void main(String[] args) {
+        /**
+         * Tests for Player class, testing the constructor, get methods, increment score methods, reset methods and creating new instances.
+         *
+         */
+        Player p = new Player("Steve", 0);  //Creating object (which inherently tests setName and setScore
+
+        System.out.println("Score: " + p.getScore());           //Testing getScore method
+        System.out.println("Name: " + p.getName());            //Testing getName method
+
+        p.increaseScore(10);    //Testing increaseScore method
+        System.out.println("Score: " + p.getScore());     //Displaying result of increment
+
+        p.resetScore();                         //Calling resetScore method to reset score
+        p.resetName();                          //Calling resetName method to reset name
+        System.out.println("Score: " + p.getScore());     //Displaying result of reset
+        System.out.println("Name: " + p.getName());      //Displaying result of reset
+
+        p.setScore(43);                     //Setting score following reset to show that setScore works
+        p.setName("Mary");                  //Setting name following reset to show that setName works
+        System.out.println("Score: " + p.getScore());     //Displaying result of setting score
+        System.out.println("Name: " + p.getName());      //Displaying result of setting name
+    }
+}
