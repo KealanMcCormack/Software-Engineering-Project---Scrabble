8bc9bcb37dd64bbe950aa24e2a8fadbc19db9352
Lukasz Filanowski <44058072+Lukadolla@users.noreply.github.com>
2020-02-14 09:32:18 +0000

    Add files via upload

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,4 @@
 /**
- * Gerard Colman - 18327576
- * Lukasz Filanowski - 18414616
- * Kealan McCormack - 18312236
  * @author Lukasz Filanowski
  * The purpose of this class is to create a player instance for the Scrabble game with
  * a name and score tied to the specific player instance.
@@ -107,4 +104,17 @@ public class Player {
         setName("");
     }
 
+    /**
+     *
+     * @param frame
+     * Getting the frame of player
+     */
+    protected Frame getFrame(Frame frame)
+    {
+        return frame;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
