88ca948c84b87a810832db291f75175326e1f8cf
Lukasz Filanowski <44058072+Lukadolla@users.noreply.github.com>
2020-02-18 16:13:23 +0000

    Updated BoardCreate method

--- src/Board.java
+++ src/Board.java
@@ -71,19 +71,29 @@ public class Board {
         {
             for(int column_count = 0; column_count < 8; column_count++)
             {
-                if((row_count == 7 && column_count == 0) || (row_count == 0 && column_count == 7) || (row_count == 0 && column_count == 0))
+                if((row_count == 1 && column_count == 5) || (row_count == 5 && column_count == 1) || (row_count == 5 && column_count == 5))
                 {
                     boardArray[row_count][column_count].setTileVal(tileVal.TripleLetter);
                 }
 
-                if(row_count == 7 && column_count == 7)
+                else if(row_count == 7 && column_count == 7)
                 {
                     boardArray[row_count][column_count].setTileVal(tileVal.Star);
                 }
 
-                if((row_count == column_count) && (row_count > 0 && row_count < 5) && (column_count > 0 && column_count < 5))
+                else if(row_count == column_count && row_count > 0 && row_count < 5)
                 {
                     boardArray[row_count][column_count].setTileVal(tileVal.DoubleWord);
+                }
+
+                else if((row_count%7 == 0) && (column_count%7 == 0) && !((row_count == 7) && (column_count == 7)))
+                {
+                    boardArray[row_count][column_count].setTileVal(tileVal.TripleWord);
+                }
+
+                else
+                {
+                    boardArray[row_count][column_count].setTileVal(tileVal.Standard);
                 }
             }
         }
@@ -97,64 +107,14 @@ public class Board {
 
     }
 
-    public String toString()
-    {
-        String output = "";
-
-        return output;
-    }
 
     public static void main(String[] args) {
         /*Board b = new Board();
         b.createBoard();
+        System.out.println(boardArray[7][0].getTileVal());
+        System.out.println(boardArray[0][7].getTileVal());
         System.out.println(boardArray[0][0].getTileVal());
-        System.out.println(boardArray[1][1].getTileVal());
-        System.out.println(boardArray[2][2].getTileVal());
-        System.out.println(boardArray[3][3].getTileVal());
-        System.out.println(boardArray[4][4].getTileVal());
-        System.out.println(boardArray[5][5].getTileVal());*/
-    }
-
-    protected boolean Connecting(int x, int y){ //returns true if a tile is found in a conjoining tile
-        if(boardArray[x-1][y].getCharacterVal() != ' '){
-            return true;
-        }
-
-        if(boardArray[x+1][y].getCharacterVal() != ' '){
-            return true;
-        }
-
-        if(boardArray[x][y+1].getCharacterVal() != ' '){
-            return true;
-        }
-
-        if(boardArray[x][y-1].getCharacterVal() != ' '){
-            return true;
-        }
-
-        return false;
-    }
-
-    protected boolean inLine(int arr[]){//Needs input of the previous
-        int x = arr.length;
-
-
-        for(int count = 0;count < arr.length;count++){
-
-        }
-        return false;
-    }
-
-    protected char placeLetter(int letterIndex, int x, int y){
-        Frame f = new Frame();
-
-        if(boardArray[x][y].getCharacterVal() == ' '){
-            throw new IllegalArgumentException("Already a tile placed on this square");
-        }
-
-        char a = f.getLetterIndex(letterIndex);
-        boardArray[x][y].setCharacterVal(f.playLetter(letterIndex));
-        return a;
+        System.out.println(boardArray[7][7].getTileVal());*/
     }
 
 }
