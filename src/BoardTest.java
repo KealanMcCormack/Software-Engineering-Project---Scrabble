a71e1d3397f1fef32ce8f1ab924435a61ec791c1
Lukasz Filanowski <44058072+Lukadolla@users.noreply.github.com>
2020-02-22 19:29:02 +0000

    Test Class

---
+++ src/BoardTest.java
@@ -0,0 +1,10 @@
+public class BoardTest {
+
+    public static void main(String[] args) {
+
+        Board b = new Board();
+        b.createBoard();
+        System.out.println(b.printBoard());
+
+    }
+}
