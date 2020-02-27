498ac9646512c13de9043dd6c1fb0c8c9bb12635
Gerard Colman <gerard.colman1@gmail.com>
2020-02-27 17:54:54 +0000

    Updated ui

--- src/UI.java
+++ src/UI.java
@@ -4,6 +4,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.image.*;
+
+import java.awt.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class UI extends Application {
 
@@ -18,5 +23,10 @@ public class UI extends Application {
         Scene scene = new Scene(lab);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public void board(Stage boardStage) throws FileNotFoundException {
+        FileInputStream inputStream = new FileInputStream("assests\board.png");
+        Image board = new Image(inputStream);
     }
 }
