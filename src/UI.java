d02c9ea50d6e8151925cf8263febb2d6783fa1a8
Lukasz Filanowski <44058072+Lukadolla@users.noreply.github.com>
2020-02-28 10:24:12 +0000

    Smooth Jazz method

--- src/UI.java
+++ src/UI.java
@@ -6,10 +6,13 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.image.*;
+import java.awt.Desktop;
+import java.net.URI;
 
 import java.awt.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.net.URL;
 
 public class UI extends Application {
 
@@ -23,10 +26,22 @@ public class UI extends Application {
         Scene scene = new Scene(lab);
         primaryStage.setScene(scene);
         primaryStage.show();
+        smoothJazz();
     }
 
     public void board(Stage boardStage) throws FileNotFoundException {
         FileInputStream inputStream = new FileInputStream("assets\\board.png");
         Image board = new Image(inputStream);
+    }
+
+    public void smoothJazz()
+    {
+        try {
+            Desktop desktop = java.awt.Desktop.getDesktop();
+            URI oURL = new URI("https://www.youtube.com/watch?v=Tv5QRmG9ST0&list=PL7nML7u-x2dyqPDhm0g8eMlCfsrEvk89H");
+            desktop.browse(oURL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
