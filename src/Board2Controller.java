6a5b812eb011d09618e21073912c8ba81fff14ca
Lukasz Filanowski <44058072+Lukadolla@users.noreply.github.com>
2020-03-25 21:48:55 +0000

    Commenting

--- src/Board2Controller.java
+++ src/Board2Controller.java
@@ -13,40 +13,40 @@ import java.util.ArrayList;
 
 public class Board2Controller {
     @FXML
-    private GridPane boardGrid;
+    private GridPane boardGrid;  //GridPane for board
     @FXML
-    private GridPane playerFrame;
+    private GridPane playerFrame;  //GridPane for frame
     @FXML
-    private TextArea outfield;
+    private TextArea outfield;  //Text output field
     @FXML
-    private TextField infield;
+    private TextField infield;  //Text input field
 
-    int cmdCutoff = 10;
-    ArrayList<String> cmdOut = new ArrayList<>();
+    int cmdCutoff = 10;  //Cutoff for number of commands displayed
+    ArrayList<String> cmdOut = new ArrayList<>();  //ArrayList of commands
 
-    public void setGridImage(int x, int y, Image img){
-        ObservableList<Node> children = boardGrid.getChildren();
+    public void setGridImage(int x, int y, Image img){   //Setting tile images onto board
+        ObservableList<Node> children = boardGrid.getChildren();  //Creating list of individual letters from words
         for (Node node : children){
             if(boardGrid.getRowIndex(node) == y && boardGrid.getColumnIndex(node) == x){
                 ImageView imgview = (ImageView) node;
-                imgview.setImage(img);
+                imgview.setImage(img);  //Setting image in ImageView contained in GridPane
             }
         }
     }
-    public void setPlayerFrame(Image img) {
-        ObservableList<Node> children = playerFrame.getChildren();
+    public void setPlayerFrame(Image img) {  //Setting tile images to frame
+        ObservableList<Node> children = playerFrame.getChildren();  //Creating list of individual letters from frame
         for(Node node : children)
         {
             ImageView iv = (ImageView) node;
-            iv.setImage(img);
+            iv.setImage(img);   //Setting image in ImageView contained in GridPane
         }
     }
-    public void getCMD(){
+    public void getCMD(){  //getting command input from TextField
         String input = "";
         input = infield.getText();
         printCMD(input);
     }
-    public void printCMD(String cmd) {
+    public void printCMD(String cmd) {  //displaying text to player in TextArea
         cmdOut.add(cmd + "\n");
         String finalOut = "";
         for (int i = 0; i < cmdOut.size(); i++) {
--- src/UI.java
+++ src/UI.java
@@ -9,21 +9,21 @@ import java.io.IOException;
 
 public class UI extends Application {
 
-    private static final int Height = 700;
+    private static final int Height = 700; //Setting stage sizes
     private static final int Width = 800;
 
     public static void main(String[] args) {
-        launch(args);
+        launch(args); //Launching UI
     }
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Board2.fxml"));
-        primaryStage.setTitle("Scrabble!");
-        primaryStage.setScene(new Scene(root,Width,Height));
-        primaryStage.setWidth(Width);
+        Parent root = FXMLLoader.load(getClass().getResource("Board2.fxml"));   //Linking fxml to UI
+        primaryStage.setTitle("Scrabble!");  //Setting title
+        primaryStage.setScene(new Scene(root,Width,Height));  //Creating scene
+        primaryStage.setWidth(Width);  //Setting dimensions
         primaryStage.setHeight(Height);
-        primaryStage.show();
+        primaryStage.show();  //Showing stage
     }
 
 }
